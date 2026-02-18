30f9873e7c3818f2a8ff89d8617c06600681f63c
Max Hale <maxhale@live.co.uk>
2026-02-18 11:58:23 +0000

    Update colourscheme

--- nvim/lua/plugins/one-monokai.lua
+++ nvim/lua/plugins/one-monokai.lua
@@ -1,18 +1,14 @@
 return {
     {
-        "sainnhe/gruvbox-material",
+        "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        config = function()
-            vim.g.gruvbox_material_background = "medium"
-            vim.g.gruvbox_material_foreground = "material"
-            vim.g.gruvbox_material_better_performance = 1
-        end,
+        opts = {},
     },
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "gruvbox-material",
+            colorscheme = "tokyonight",
         },
     },
 }
